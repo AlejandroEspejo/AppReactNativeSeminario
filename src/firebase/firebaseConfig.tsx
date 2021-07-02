4745cd6cd091c580baa85f78b7611a9a72ed46e9
Ditmar <ditmarcastro@gmail.com>
2021-07-02 12:01:48 -0400

    send data

---
+++ src/firebase/firebaseConfig.tsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZem-K3sAJ6dkwf97zeeFKbzjT3K6SFc0",
+    authDomain: "seminario-1e3b7.firebaseapp.com",
+    projectId: "seminario-1e3b7",
+    storageBucket: "seminario-1e3b7.appspot.com",
+    messagingSenderId: "176320771168",
+    appId: "1:176320771168:web:c621fd7d90e37a140d3f05",
+    measurementId: "G-YD0C0FWVQ4"
+  };
+export default firebaseConfig;
